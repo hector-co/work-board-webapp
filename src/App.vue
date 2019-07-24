c043321fe4167f4001a1d01a21b05d494b13a010
Hector Carballo <hectorcarballo.hco@gmail.com>
2019-07-23 23:09:09 -0400

    adding bootstrap vue and basic boards list

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,25 @@
 <template>
   <div id="app">
-    <nav class="navbar navbar-expand-sm navbar-light bg-light">
-      <router-link to="/">
-        <a class="navbar-brand">WorkBoard</a>
-      </router-link>
+    <b-navbar toggleable="lg" type="dark" variant="info">
+      <b-navbar-brand to="/">WorkBoard</b-navbar-brand>
+      <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+      <b-collapse id="nav-collapse" is-nav>
+        <b-navbar-nav>
+          <b-nav-item to="/boards">Boards</b-nav-item>
+        </b-navbar-nav>
+      </b-collapse>
+    </b-navbar>
 
-      <div class="collapse navbar-collapse">
-        <ul class="navbar-nav">
-          <li class="nav-item">
-            <router-link to="/boards">
-              <a class="nav-link">Boards</a>
-            </router-link>
-          </li>
-        </ul>
-      </div>
-    </nav>
-
-    <div class="container">
-      <div class="row">
-        <div class="col-sm">
+    <b-container>
+      <b-row>
+        <b-col>
           <router-view />
-        </div>
-      </div>
-    </div>
+        </b-col>
+      </b-row>
+    </b-container>
   </div>
 </template>
 <style lang="scss">
 @import "bootstrap/scss/bootstrap.scss";
+@import "bootstrap-vue/src/index.scss";
 </style>
--- src/views/BoardList.vue
+++ src/views/BoardList.vue
@@ -1,10 +1,24 @@
 <template>
   <div class="board-list">
-    <table>
-      <tr v-for="board in data" :key="board.id">
-        <td>{{board.id}}</td>
-        <td>{{board.title}}</td>
-      </tr>
+    <h3>Boards</h3>
+    <table class="table b-table table-striped table-hover">
+      <thead>
+        <tr>
+          <th width="75px">Code</th>
+          <th>Title</th>
+          <th width="150px"></th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="board in data" :key="board.id">
+          <td>{{board.id}}</td>
+          <td>{{board.title}}</td>
+          <td>
+            <b-button size="sm">
+            </b-button>
+          </td>
+        </tr>
+      </tbody>
     </table>
   </div>
 </template>
